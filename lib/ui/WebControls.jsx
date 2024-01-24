1ac370ca8cf6519f4881e2faebf8d553c170e9dc
kevin.talmadge@eluv.io <kevin@eluv.io>
2024-01-24 11:55:26 -0800

    Menu

--- lib/ui/WebControls.jsx
+++ lib/ui/WebControls.jsx
@@ -2,12 +2,11 @@ import React, {useEffect, useState} from "react";
 import ControlStyles from "../static/stylesheets/controls-web.module.scss";
 import * as Icons from "../static/icons/Icons.js";
 import {ObserveVideo} from "./Observers.js";
-import "focus-visible";
 
 // TODO: Move stuff to general components class
 const IconButton = ({icon, ...props}) => {
   return (
-    <button dangerouslySetInnerHTML={{__html: icon}} {...props} />
+    <button dangerouslySetInnerHTML={{__html: icon}} {...props} className={`${ControlStyles["icon-button"]} ${props.className || ""}`} />
   );
 };
 
@@ -29,9 +28,54 @@ const Time = (time, total) => {
   return string;
 };
 
+const SettingsMenu = ({player}) => {
+  const [setting, setSetting] = useState(undefined);
+  const [options, setOptions] = useState(undefined);
+
+  useEffect(() => {
+    console.log("Updating options")
+    setOptions({
+      qualityLevels: player.controls.GetQualityLevels(),
+      audioTracks: player.controls.GetAudioTracks(),
+      textTracks: player.controls.GetTextTracks(),
+      playerProfiles: player.controls.GetPlayerProfiles()
+    });
+  }, [player.__updateIndex]);
+
+  if(!options) { return null; }
+
+  console.log(options);
+  return (
+    <div className={`${ControlStyles["menu"]} ${ControlStyles["settings-menu"]}`}>
+      <button onClick={() => setSetting("quality")} className={ControlStyles["menu-option"]}>
+        { options.qualityLevels.active.activeLabel }
+      </button>
+      {
+        options.audioTracks.options.length <= 1 ? null :
+          <button onClick={() => setSetting("audio")} className={ControlStyles["menu-option"]}>
+            { options.audioTracks.active.activeLabel }
+          </button>
+      }
+      {
+        options.textTracks.options.length <= 1 ? null :
+          <button onClick={() => setSetting("text")} className={ControlStyles["menu-option"]}>
+            { options.textTracks.active.activeLabel }
+          </button>
+      }
+      {
+        options.playerProfiles.options.length === 0 ? null :
+          <button onClick={() => setSetting("profile")} className={ControlStyles["menu-option"]}>
+            {options.playerProfiles.active.activeLabel}
+          </button>
+      }
+    </div>
+  );
+};
+
 const WebControls = ({player, dimensions, className=""}) => {
   const [videoState, setVideoState] = useState(undefined);
   const [currentTime, setCurrentTime] = useState(player.video.currentTime);
+  const [showSettingsMenu, setShowSettingsMenu] = useState(false);
 
   useEffect(() => {
     const RemoveObserver = ObserveVideo({target: player.target, video: player.video, setVideoState, setCurrentTime});
@@ -42,7 +86,7 @@ const WebControls = ({player, dimensions, className=""}) => {
   if(!videoState) { return null; }
 
   return (
-    <div className={`${className} ${ControlStyles["container"]} ${ControlStyles[`size-${dimensions.size}`]} ${ControlStyles[`orientation-${dimensions.orientation}`]}`}>
+    <div className={`${className} ${ControlStyles["container"]} ${ControlStyles[`size-${dimensions.size}`] || ""} ${ControlStyles[`orientation-${dimensions.orientation}`] || ""}`}>
       <div className={ControlStyles["progress-container"]}>
         <progress
           max={1}
@@ -101,6 +145,17 @@ const WebControls = ({player, dimensions, className=""}) => {
           { Time(currentTime, videoState.duration) } / { Time(videoState.duration, videoState.duration) }
         </div>
         <div className={ControlStyles["spacer"]} />
+        <div className={ControlStyles["menu-control-container"]}>
+          <IconButton
+            aria-label={showSettingsMenu ? "Hide Settings Menu" : "Settings"}
+            icon={Icons.SettingsIcon}
+            onClick={() => setShowSettingsMenu(!showSettingsMenu)}
+          />
+          {
+            !showSettingsMenu ? null :
+              <SettingsMenu player={player} />
+          }
+        </div>
         <IconButton
           aria-label={videoState.fullscreen ? "Exit Fullscreen" : "Fullscreen"}
           icon={videoState.fullscreen ? Icons.ExitFullscreenIcon : Icons.FullscreenIcon}
