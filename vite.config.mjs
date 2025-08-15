df136530cec2aec695a21c760568c767fa1d7d4d
Kevin Talmadge <kevin@eluv.io>
2025-08-14 17:01:39 -0700

    Update client and vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,17 +1,11 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import path from "path";
 import autoprefixer from "autoprefixer";
 import react from "@vitejs/plugin-react-swc";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 let plugins = [
-  react(),
-  splitVendorChunkPlugin()
+  react()
 ];
-
-if(process.env.SERVE_HTTPS) {
-  plugins.push(basicSsl());
-}
 
 export default defineConfig(() => {
   return {
@@ -21,14 +15,6 @@ export default defineConfig(() => {
       host: true
     },
     build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (chunkInfo) => {
-            if(chunkInfo.name === "style.css")
-              return "elv-player-js.css";
-          },
-        },
-      },
       lib: {
         entry: path.resolve(__dirname, "lib/index.js"),
         name: "@eluvio/elv-player-js",
